f82d1ea7d7210e549d6034513c8e177abace7520
LogicIsPlantingBomb <logicunit64@gmail.com>
2025-03-07 20:20:00 +0530

    anothercommit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import useState from 'react';
+function App() {
+  return (
+	  <div>
+	  	<h1 className="bg-zinc-600">hello</h1>
+	  </div>
+  );
+}
+export default App;
